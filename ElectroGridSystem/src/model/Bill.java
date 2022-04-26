b79c796a2a5d56efcc768d747ed760be71afa4ba
Tharindu Dilshan A.D <91265521+TharinduDilshan99@users.noreply.github.com>
2022-04-26 14:36:07 +0530

    Update Bill.java
    read most relevant bill details

--- ElectroGridSystem/src/model/Bill.java
+++ ElectroGridSystem/src/model/Bill.java
@@ -186,5 +186,87 @@ public class Bill {
 		 
 	    	 return output; 
 		 }
+	
+	
+	       //read most relevant bill details
+		
+		public String readOneBill()
+		{
+		String output = "";
+		try
+		{
+		Connection con = connect();
+		if (con == null)
+		{
+		return "Error while connecting to the database for reading...";
+		}
+		// Prepare the view table to be displayed
+		output = "<table border='1'>"
+
+		+ "<tr>"
+		+ "<th>Bill ID</th>"
+		+ "<th>User Name</th>"
+		+ "<th>User Address</th>"
+		+ "<th>Unit Count</th>"
+		+ "<th>Bill Amount</th>"
+		+ "<th>Due Amount</th>"
+		+ "<th>Date</th>"
+		+ "<th>Update</th>"
+		+ "<th>Remove</th>"
+		+ "</tr>";
+
+
+
+		String query = "select * from bill where BillID= (Select max(BillID) from bill)";
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		
+		
+		// iterate through the rows in the result set
+		
+		while (rs.next())
+		{
+
+		String BillID = rs.getString("BillID");
+		String UserName = rs.getString("UserName");
+		String UserAddress = rs.getString("UserAddress");
+		String UnitCount = rs.getString("UnitCount");
+		String BillAmount = rs.getString("BillAmount");
+		String DueAmount = rs.getString("DueAmount");
+		String Date = rs.getString("Date");
+		
+		
+		// Add into the html table
+		
+		output += "<tr><td>" + BillID + "</td>";
+		output += "<td>" + UserName + "</td>";
+		output += "<td>" + UserAddress + "</td>";
+		output += "<td>" + UnitCount + "</td>";
+		output += "<td>" + BillAmount + "</td>";
+		output += "<td>" + DueAmount + "</td>";
+		output += "<td>" + Date + "</td>";
+		
+		
+		// buttons
+		
+		output += "<td>" + "<input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'>" + "</td>"
+		+ "<td>" + " <input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" + "</td></tr>"
+
+		+"<input name='BillID' type='hidden' value='" + BillID + "'>" + "</form></td></tr>";
+		}
+		con.close();
+		
+		// Complete the html table
+		
+		output += "</table>";
+		}
+		
+		catch (Exception e)
+		{
+		output = "Error while reading the items.";
+		System.err.println(e.getMessage());
+		}
+		return output;
+		}
 
 }
